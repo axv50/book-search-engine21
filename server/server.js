e1bfee7f6f8ecfab51a1364b841cea2dd99330d2
axv50 <alexvega50@gmail.com>
2021-10-15 12:47:40 -0500

    done

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+app.use(cors());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// db.once("open", () => {
+app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
